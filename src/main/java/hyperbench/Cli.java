42e29925aa096f0bdc729fc520989710830fdf38
Arvind Jayaprakash <work@anomalizer.net>
2011-06-19 23:12:58 +0530

    switch to log4j

--- src/main/java/hyperbench/Cli.java
+++ src/main/java/hyperbench/Cli.java
@@ -1,13 +1,8 @@
 package hyperbench;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.net.URL;
-
 public class Cli {
     public static void main(String args[]) throws InterruptedException {
-        LoadSet l = new SimpleGet("http://localhost:81/STL_doc/", 30*1000);
+        LoadSet l = new SimpleGet("http://localhost:81/STL_doc/", 30 * 1000);
 
         Thread t = new Thread(new Harness(l.iterator(), 100), "load generator");
         t.start();
