fb4fc6b08d36ab7b8fc028d59321cd14c862cd55
Arvind Jayaprakash <work@anomalizer.net>
2011-06-20 23:48:10 +0530

    Command line args for all that was hard-coded

--- src/main/java/hyperbench/Cli.java
+++ src/main/java/hyperbench/Cli.java
@@ -1,10 +1,21 @@
 package hyperbench;
 
+import com.beust.jcommander.JCommander;
+
 public class Cli {
     public static void main(String args[]) throws InterruptedException {
-        LoadSet l = new SimpleGet("http://localhost:81/STL_doc/", 30 * 1000);
 
-        Thread t = new Thread(new Harness(l.iterator(), 100), "load generator");
+        CliArgs opts = new CliArgs();
+        JCommander jc = new JCommander(opts, args);
+
+        if (args.length == 0) {
+            jc.usage();
+            return;
+        }
+
+        LoadSet l = new SimpleGet(opts.url, opts.requests);
+
+        Thread t = new Thread(new Harness(l.iterator(), opts.concurrency), "load generator");
         t.start();
         t.join();
     }
---
+++ src/main/java/hyperbench/CliArgs.java
@@ -0,0 +1,17 @@
+package hyperbench;
+
+import com.beust.jcommander.Parameter;
+
+/**
+ */
+public class CliArgs {
+
+    @Parameter(names = "-u", description = "The url to be invoked")
+    public String url;
+
+    @Parameter(names = "-n", description = "Total number of requests to be issued")
+    public int requests = 1;
+
+    @Parameter(names = "-c", description = "Max concurrency")
+    public short concurrency = 1;
+}
