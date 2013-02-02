5149f5b3995abe16971633b733620db14c30cf83
Arvind Jayaprakash <work@anomalizer.net>
2013-02-02 17:48:24 +0530

    lombokification

--- src/main/java/hyperbench/input/HttpRequestPrototype.java
+++ src/main/java/hyperbench/input/HttpRequestPrototype.java
@@ -3,6 +3,7 @@ package hyperbench.input;
 import hyperbench.stats.AveragingRequestGroupTracker;
 import hyperbench.stats.RequestGroupTracker;
 import hyperbench.stats.RequestTracker;
+import lombok.Getter;
 import org.jboss.netty.buffer.ChannelBuffer;
 import org.jboss.netty.buffer.ChannelBuffers;
 import org.jboss.netty.handler.codec.http.DefaultHttpRequest;
@@ -22,11 +23,11 @@ import java.net.UnknownHostException;
 public class HttpRequestPrototype {
     private static final Logger logger = LoggerFactory.getLogger(HttpRequestPrototype.class);
 
-    private InetAddress addr = null;
-    private String host = null;
-    private HttpRequest request;
-    private int port;
-    private String uriString;
+    @Getter private InetAddress hostAddress = null;
+    @Getter private String host = null;
+    @Getter private HttpRequest httpRequest;
+    @Getter private int port;
+    @Getter private String uriString;
 
     private final RequestGroupTracker stats = new AveragingRequestGroupTracker();
 
@@ -49,15 +50,15 @@ public class HttpRequestPrototype {
     public void setUrl(String urlString, HttpMethod method) throws URISyntaxException, UnknownHostException {
         URI url = new URI(urlString);
         host = url.getHost();
-        addr = InetAddress.getByName(host);
+        hostAddress = InetAddress.getByName(host);
         uriString = urlString;
         port = url.getPort();
         if(port == -1)
             port = 80;
 
-        request = new DefaultHttpRequest(HttpVersion.HTTP_1_1, method, getUriString());
-        request.setHeader(HttpHeaders.Names.HOST, getHost());
-        request.setHeader(HttpHeaders.Names.CONNECTION, HttpHeaders.Values.CLOSE);
+        httpRequest = new DefaultHttpRequest(HttpVersion.HTTP_1_1, method, getUriString());
+        httpRequest.setHeader(HttpHeaders.Names.HOST, getHost());
+        httpRequest.setHeader(HttpHeaders.Names.CONNECTION, HttpHeaders.Values.CLOSE);
 
         String protocol = url.getScheme();
         if( !("http".equals(protocol)) ) {
@@ -65,29 +66,9 @@ public class HttpRequestPrototype {
         }
     }
 
-    public InetAddress getHostAddress() {
-        return addr;
-    }
-
-    public String getHost() {
-        return host;
-    }
-
-    public int getPort() {
-        return port;
-    }
-
-    public String getUriString() {
-        return uriString;
-    }
-
-    public HttpRequest getHttpRequest() {
-        return request;
-    }
-
     public void addHeader(String name, String value) {
-        if(request != null) {
-            request.addHeader(name, value);
+        if(httpRequest != null) {
+            httpRequest.addHeader(name, value);
         }
     }
 
@@ -101,15 +82,15 @@ public class HttpRequestPrototype {
 
 
     public void setBody(Object body) {
-        if(request != null) {
+        if(httpRequest != null) {
             ChannelBuffer cb = null;
             if(body instanceof String) {
                 cb = ChannelBuffers.wrappedBuffer(((String) body).getBytes());
             } else {
                 cb = ChannelBuffers.wrappedBuffer((byte[]) body);
             }
-            request.setHeader("Content-Length", cb.array().length);
-            request.setContent(cb);
+            httpRequest.setHeader("Content-Length", cb.array().length);
+            httpRequest.setContent(cb);
         }
     }
 }
