511f67382dd409695feabb8c9ab9c89667991b91
Arvind Jayaprakash <work@anomalizer.net>
2013-02-02 20:18:01 +0530

    finally works again

--- src/main/java/hyperbench/netty/HttpClientPipelineFactory.java
+++ src/main/java/hyperbench/netty/HttpClientPipelineFactory.java
@@ -4,14 +4,18 @@ import hyperbench.request.HttpResponseHandler;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelPipeline;
 import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.http.HttpChunkAggregator;
 import io.netty.handler.codec.http.HttpClientCodec;
 
 public class HttpClientPipelineFactory extends ChannelInitializer<SocketChannel> {
 
+    public static final int MAX_CONTENT_LENGTH = 1024 * 1024;
+
     @Override
     public void initChannel(SocketChannel ch) throws Exception {
         ChannelPipeline pipeline = ch.pipeline();
-        pipeline.addLast("codec", new HttpClientCodec());
+        pipeline.addLast("http", new HttpClientCodec());
+        pipeline.addLast("aggregator", new HttpChunkAggregator(MAX_CONTENT_LENGTH));
         pipeline.addLast("handler", new HttpResponseHandler());
     }
 }
