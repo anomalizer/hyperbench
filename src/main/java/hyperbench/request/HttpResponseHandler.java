fe0381345a5b70eafea1534361be9e650f19ad8d
Arvind Jayaprakash <work@anomalizer.net>
2013-03-31 17:12:28 +0530

    trace logging provides http response

--- src/main/java/hyperbench/request/HttpResponseHandler.java
+++ src/main/java/hyperbench/request/HttpResponseHandler.java
@@ -3,13 +3,15 @@ package hyperbench.request;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundMessageHandlerAdapter;
-import io.netty.handler.codec.http.HttpResponse;
+import io.netty.handler.codec.http.FullHttpResponse;
 import lombok.extern.slf4j.Slf4j;
+
+import java.util.List;
 
 /**
  */
 @Slf4j
-public class HttpResponseHandler extends ChannelInboundMessageHandlerAdapter<HttpResponse> {
+public class HttpResponseHandler extends ChannelInboundMessageHandlerAdapter<FullHttpResponse> {
     @Override
     public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
         log.warn("Aiee, got an exception", cause);
@@ -20,8 +22,12 @@ public class HttpResponseHandler extends ChannelInboundMessageHandlerAdapter<Htt
     }
 
     @Override
-    public void messageReceived(ChannelHandlerContext ctx, HttpResponse msg) throws Exception {
-        log.trace("Received response {}", msg);
+    public void messageReceived(ChannelHandlerContext ctx, FullHttpResponse msg) throws Exception {
+        if(log.isTraceEnabled()) {
+            final List<String> encodingHeader = msg.headers().getAll("Content-Encoding");
+            final String encoding = (encodingHeader.size() == 1) ? encodingHeader.get(0): "utf-8";
+            log.trace("Received response {}", new String(msg.data().array(), encoding));
+        }
         HttpRequestContext rc = ctx.attr(Harness.STATE).get();
         if(rc != null) {
             rc.getTracker().recordResponse(msg.getStatus().code());
